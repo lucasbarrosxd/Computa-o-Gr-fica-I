cf6dcab5e9d4d572f676bec31f6f0d1806020139
Victor Dantas <dantas.victor94@gmail.com>
2019-09-01 15:51:21 -0300

    Adicionando a função de mudar a cor de um pixel

--- Trabalhos/Trabalho_4.py
+++ Trabalhos/Trabalho_4.py
@@ -7,14 +7,19 @@ def newImage(mode, size, color=0):
     return im
 
 mode = 'RGBA'
-size = (640, 480)
-color = (73, 109, 137)
+size = (600, 600)
+color = (0, 0, 0)
 
 im = newImage(mode=mode, size=size, color=color)
 
+draw =ImageDraw.Draw(im)
+white = (255, 255, 255)
+for i in range(0,600):
+    draw.point((i, i), white)
+    draw.point((600-i, i), white)
 
 image_path = '/home/bit/Documentos/2019.2/Computa-o-Gr-fica-I'
 image_name = 'imagemInicial.png'
-im.save(image_path + image_name )
+im.save(image_path + image_name)
 
 im.show()
