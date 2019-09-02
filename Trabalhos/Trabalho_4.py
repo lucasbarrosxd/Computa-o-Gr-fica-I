8db11089b7c373a2953521684f62ddfe462d1a34
Lucas Barros <lucasbarrosxd21@gmail.com>
2019-09-01 22:15:04 -0300

    Verde
    Verde

--- Trabalhos/Trabalho_4.py
+++ Trabalhos/Trabalho_4.py
@@ -1,24 +1,34 @@
-from PIL import *
+from python.raycaster.physics import *
+from python.raycaster.observer import Scenery, Observer
+from python.raycaster.renderable.r3d import Cone
 from PIL import Image, ImageDraw
 
-def newImage(mode, size, color=0):
 
-    im = Image.new(mode, size, color)
-    return im
+def new_image(mode, size, color=0):
+    return Image.new(mode, size, color)
 
-mode = 'RGBA'
-size = (600, 600)
-color = (0, 0, 0)
 
-im = newImage(mode=mode, size=size, color=color)
-
-draw =ImageDraw.Draw(im)
+mode = 'RGB'
 white = (255, 255, 255)
-for i in range(0,600):
-    draw.point((i, i), white)
-    draw.point((600-i, i), white)
+green = (46, 204, 113)
+black = 0
+res = (1920, 1080)
 
-image_path = '/home/bit/Documentos/2019.2/Computa-o-Gr-fica-I'
+im = new_image(mode=mode, size=res, color=black)
+draw = ImageDraw.Draw(im)
+
+# Fazer o raycast.
+cone = Cone(Point(10, 0, 0), Point(10, 5, 0), 2)
+
+scenery = Scenery(Observer.A(Point(0, 0, 0), Point(5, 0, 0), res, size=(10, 10)))
+scenery.add_obj(cone, "cone", green)
+
+for index_x in range(res[0]):
+    for index_y in range(res[1]):
+        if scenery.shoot(index_x, res[1] - index_y - 1):
+            draw.point((index_x, index_y), green)
+
+image_path = '../Debug/'
 image_name = 'imagemInicial.png'
 im.save(image_path + image_name)
 
