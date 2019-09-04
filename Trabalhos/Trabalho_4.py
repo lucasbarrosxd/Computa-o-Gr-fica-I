0d20620e6cc42c9aa6148f9f4d56e0501e1c2c72
Lucas Barros <lucasbarrosxd21@gmail.com>
2019-09-04 18:38:29 -0300

    Testes no Cenário
    Foram feitos alguns testes com objetos no cenário. Adicionada a pasta
    Debug ao .gitignore.
    
    Co-authored-by: Victor Dantas <dantas.victor94@gmail.com>

--- Trabalhos/Trabalho_4.py
+++ Trabalhos/Trabalho_4.py
@@ -10,30 +10,47 @@ import time
 mode = 'RGB'
 white = (255, 255, 255)
 black = (0, 0, 0)
-blue = (50, 50, 200)
-green = (46, 204, 113)
+light_blue = (0, 181, 204)
+dark_blue = (50, 50, 200)
+light_green = (46, 204, 113)
+dark_green = (24, 102, 56)
 red = (255, 0, 0)
 brown = (247, 202, 24)
 
 # Fazer o raycast.
-res = (300, 300)
+res = (500, 500)
 observer_1 = Observer.A(Point(0, 0, 0), Point(5, 0, 0), res, size=(10, 10))
 observer_2 = Observer.A(Point(0, 2.5, 0), Point(5, 2.25, 0), res, size=(8, 8))
+observer_t = Observer.A(Point(5, 5, 30), Point(5, 5, 27), res, size=(7, 7))
 
-observer = observer_2
+observer = observer_t
 
-im = Image.new(mode=mode, size=res, color=black)
+im = Image.new(mode=mode, size=res, color=light_blue)
 draw = ImageDraw.Draw(im)
 
-scene = Scene()
-cyli = Cylinder.B(Point(10, -5, 0), Point(10, 2, 0), 5)
-cone = Cone(Point(10, 0, 0), Point(10, 5, 0), 2)
+# Objetos Teste
+sphere_1 = Sphere(Point(5, 2.5, 5), 2.5)
+sphere_2 = Sphere(Point(5, 7.5, 5), 2.5)
+sphere_3 = Sphere(Point(5, 12.5, 5), 2.5)
 plane = Plane(Point(0, 0, 0), Vector(0, 1, 0))
-sphere = Sphere(Point(10, 2, 0), 2)
 triangle = Triangle(Point(8, 0, 0), Point(7.5, 2, 1), Point(7, 0, 2))
-scene.add_obj(sphere, "ball", blue)
-scene.add_obj(plane, "ground", green)
-scene.add_obj(triangle, "triangle", red)
+# Objetos Trabalho
+cyli_1 = Cylinder.B(Point(2, 0, 8), Point(2, 7, 8), 1)
+cyli_2 = Cylinder.B(Point(7, 0, 8), Point(7, 7, 8), 1)
+cone_1 = Cone(Point(2, 7, 8), Point(2, 13, 8), 1.5)
+cone_2 = Cone(Point(7, 7, 8), Point(7, 13, 8), 1.5)
+
+# Cenário
+scene = Scene()
+#scene.add_obj(plane, "ground", light_green)
+#scene.add_obj(sphere_1, "building_1", red)
+#scene.add_obj(sphere_2, "building_2", white)
+#scene.add_obj(sphere_3, "building_3", black)
+scene.add_obj(cyli_1, "tree_trunk_1", brown)
+#scene.add_obj(cone_1, "tree_leaves_1", dark_green)
+scene.add_obj(cyli_2, "tree_trunk_2", brown)
+#scene.add_obj(cone_2, "tree_leaves_2", dark_green)
+
 
 for x_index in range(observer.panel.res[1]):
     for y_index in range(observer.panel.res[0]):
